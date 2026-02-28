f1761f47877a7f6b95f0351d685c468da91e845c
Dhayalan Nadar <dhayalan.nadar@liaisonbank.com>
2026-02-28 17:13:00 +0530

    Ongoing and Completed updated

--- app/completed-liaison-bank/page.js
+++ app/completed-liaison-bank/page.js
@@ -5,10 +5,10 @@ import useBodyClass from '@/components/useBodyClass'; // Adjust path as needed
 import PageTitleWave from '@/components/PageTitleWave';
 
 export default function CompletedLiaisonbankPage() {
-   useBodyClass('completed');
+  useBodyClass('completed');
   return (
     <>
-     <div className="page-header">
+      <div className="page-header">
         <div className="inner-header">
           <PageTitleWave />
           <div className="page-title">
@@ -45,37 +45,90 @@ export default function CompletedLiaisonbankPage() {
       </div>
       <section className="container py-5">
         <div className="row justify-content-center text-center">
-          <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-            <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black">
-              To get started, edit the page.js file.
-            </h1>
-            <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-              Looking for a starting point or more instructions? Head over to{" "}
-              <a
-                href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                className="font-medium text-zinc-950"
-              >
-                Templates
-              </a>{" "}
-              or the{" "}
-              <a
-                href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                className="font-medium text-zinc-950"
-              >
-                Learning
-              </a>{" "}
-              center.
-            </p>
-          </div>
-          <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-            <a
-              className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-              href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Documentation
-            </a>
+          <div className="col-12">
+            <div className="table-container">
+              <table className="project-table border table-bordered w-75 mx-auto completed">
+                <thead>
+                  <tr>
+                    <th>Sr. No</th>
+                    <th>Client Name</th>
+                    <th>Location</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr><td>1</td><td>Geeta Clinic</td><td>Dadar (W)</td></tr>
+                  <tr><td>2</td><td>Geeta Clinic</td><td>Mahim (W)</td></tr>
+                  <tr><td>3</td><td>Khandeparkar Nursing Home</td><td>Dadar (E)</td></tr>
+                  <tr><td>4</td><td>Poush Restaurant</td><td>Malad (W)</td></tr>
+                  <tr><td>5</td><td>Pizza Hut</td><td>Malad (W)</td></tr>
+                  <tr><td>6</td><td>1441 Pizzeria</td><td>Malad (W)</td></tr>
+                  <tr><td>7</td><td>Café Venture</td><td>Malad (W)</td></tr>
+                  <tr><td>8</td><td>Ferry Warf</td><td>Malad (W)</td></tr>
+                  <tr><td>9</td><td>Sammy Sosa</td><td>Oshiwara</td></tr>
+                  <tr><td>10</td><td>Persia Darbar (Shop 13 &amp; 14)</td><td>Malad (W)</td></tr>
+                  <tr><td>11</td><td>Persia Darbar (Near Kotkar Industry)</td><td>Goregaon (E)</td></tr>
+                  <tr><td>12</td><td>LIC Building</td><td>Borivali (W)</td></tr>
+                  <tr><td>13</td><td>WokExpress</td><td>Malad (W)</td></tr>
+                  <tr><td>14</td><td>Godrej Café – The Club (Gym)</td><td>Vikhroli (W)</td></tr>
+                  <tr><td>15</td><td>Godrej – The Club Restaurant</td><td>Vikhroli (W)</td></tr>
+                  <tr><td>16</td><td>Zebro Foods</td><td>Chembur (E)</td></tr>
+                  <tr><td>17</td><td>Bombay Bay Leaf</td><td>Andheri</td></tr>
+                  <tr><td>18</td><td>D’ Monte Park</td><td>Bandra (W)</td></tr>
+                  <tr><td>19</td><td>Sunil Kowli’s Surgical Clinic</td><td>Dadar (W)</td></tr>
+                  <tr><td>20</td><td>Sunaina Eye Care Centre</td><td>Dadar (W)</td></tr>
+
+                  <tr><td>21</td><td>Fatty Bao</td><td>Andheri (W)</td></tr>
+                  <tr><td>22</td><td>Nawabia Caterers</td><td>Grant Road</td></tr>
+                  <tr><td>23</td><td>Pallavi Restaurant</td><td>Goregaon (E)</td></tr>
+                  <tr><td>24</td><td>Ruchi Food</td><td>Jogeshwari (W)</td></tr>
+                  <tr><td>25</td><td>Shalimar</td><td>Jogeshwari (W)</td></tr>
+                  <tr><td>26</td><td>Rigmor Coffee</td><td>Vikhroli (E)</td></tr>
+                  <tr><td>27</td><td>Rigmor Coffee</td><td>Bandra (E)</td></tr>
+                  <tr><td>28</td><td>Sigdi The Restaurant</td><td>Bandra (W)</td></tr>
+                  <tr><td>29</td><td>Sigdi The Restaurant</td><td>Bandra (W)</td></tr>
+                  <tr><td>30</td><td>Dian Beverage &amp; Foods</td><td>Malad (W)</td></tr>
+                  <tr><td>31</td><td>Bollyfood Cafe</td><td>Powai</td></tr>
+                  <tr><td>32</td><td>Persia Darbar</td><td>Andheri (W)</td></tr>
+                  <tr><td>33</td><td>Persia Darbar</td><td>Jogeshwari (W)</td></tr>
+                  <tr><td>34</td><td>Persian Darbar</td><td>Byculla</td></tr>
+                  <tr><td>35</td><td>Persian Darbar Takeaway</td><td>Byculla</td></tr>
+                  <tr><td>36</td><td>Dr. Keiki Mehta International Eye Hospital</td><td>Colaba</td></tr>
+                  <tr><td>37</td><td>Hotel Supreme</td><td>Colaba</td></tr>
+                  <tr><td>38</td><td>Water Front (Mittal Builders)</td><td>Colaba</td></tr>
+                  <tr><td>39</td><td>Dr. Animesh Gandhi (Delight Multi-Specialty Hospital)</td><td>Wadala</td></tr>
+                  <tr><td>40</td><td>Jaslok Hospital</td><td>Peddar Road</td></tr>
+
+                  <tr><td>41</td><td>H.K. Export India Pvt. Ltd. – Zest</td><td>Worli</td></tr>
+                  <tr><td>42</td><td>Oriental Palace</td><td>Bandra (W)</td></tr>
+                  <tr><td>43</td><td>Hotel Mini Punjab</td><td>Khar (W)</td></tr>
+                  <tr><td>44</td><td>Saheb Restaurant (Hill Road)</td><td>Bandra (W)</td></tr>
+                  <tr><td>45</td><td>Marks &amp; Spencer</td><td>Bandra (W)</td></tr>
+                  <tr><td>46</td><td>World of Titan</td><td>Bandra (W)</td></tr>
+                  <tr><td>47</td><td>Puja Casa</td><td>Bandra (W)</td></tr>
+                  <tr><td>48</td><td>Hotel Alfa</td><td>Pali Hill, Bandra (W)</td></tr>
+                  <tr><td>49</td><td>Paneri Villa</td><td>Bandra (W)</td></tr>
+
+                  <tr><td>50</td><td>5 Spice</td><td>Bandra (W)</td></tr>
+                  <tr><td>51</td><td>5 Spice</td><td>Santacruz (W)</td></tr>
+                  <tr><td>52</td><td>5 Spice</td><td>Malad (W)</td></tr>
+                  <tr><td>53</td><td>5 Spice</td><td>Oshiwara</td></tr>
+                  <tr><td>54</td><td>5 Spice</td><td>Andheri (W)</td></tr>
+                  <tr><td>55</td><td>5 Spice</td><td>Khar (W)</td></tr>
+
+                  <tr><td>56</td><td>Domino’s Pizza</td><td>Malad (W)</td></tr>
+                  <tr><td>57</td><td>Domino’s Pizza</td><td>Vile Parle (W)</td></tr>
+                  <tr><td>58</td><td>Domino’s Pizza</td><td>Wadala</td></tr>
+                  <tr><td>59</td><td>Domino’s Pizza</td><td>Kandivali (W)</td></tr>
+                  <tr><td>60</td><td>Domino’s Pizza</td><td>Kandivali (E) / Govandi</td></tr>
+
+                  <tr><td>121</td><td>Vinanajay</td><td>Santacruz (E)</td></tr>
+                  <tr><td>122</td><td>Piramal Mahalaxmi</td><td>Mahalaxmi / Byculla</td></tr>
+                  <tr><td>123</td><td>Piramal Arayana</td><td>Mulund</td></tr>
+                  <tr><td>124</td><td>Piramal Revanta</td><td>Mulund</td></tr>
+                  <tr><td>125</td><td>Piramal Vaikunth</td><td>Thane</td></tr>
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </section>
--- app/ongoing-liaison-bank/page.js
+++ app/ongoing-liaison-bank/page.js
@@ -3,7 +3,7 @@
 import Link from "next/link";
 import useBodyClass from '@/components/useBodyClass'; // Adjust path as needed
 import PageTitleWave from '@/components/PageTitleWave';
-import OngoingList from "@/components/OngoingList/page";
+// import OngoingList from "@/components/OngoingList/page";
 
 export default function OngoingLiaisonbankPage() {
   useBodyClass('ongoing');
@@ -46,34 +46,72 @@ export default function OngoingLiaisonbankPage() {
       <section className="container py-5">
         <div className="row justify-content-center text-center">
           <div className="col-12">
-              <OngoingList />
-            <ul className="d-none">
-              <li><div className="client-name">Anubhav Restaurants</div> <span className="location-tag">Fort</span></li>
-              <li><div className="client-name">South Indian Restaurants</div> <span className="location-tag">Fort</span></li>
-              <li><div className="client-name">National Hindu Hotel</div> <span className="location-tag">Fort</span></li>
-              <li><div className="client-name">Durga Bar and Restaurant</div> <span className="location-tag">Andheri (E)</span></li>
-              <li><div className="client-name">Saifee Hospital</div> <span className="location-tag">Charni Road</span></li>
-              <li><div className="client-name">Amar Fruit Juice Centre</div> <span className="location-tag">Vile Parle(W)</span></li>
-              <li><div className="client-name">Amber Restaurant</div> <span className="location-tag">Bandra(W)</span></li>
-              <li><div className="client-name">Urban Tadka</div> <span className="location-tag">Malad (W)</span></li>
-              <li><div className="client-name">Hotel Silver Inn</div> <span className="location-tag">Andheri (E)</span></li>
-              <li><div className="client-name">Shree Ambe</div> <span className="location-tag">Andheri (E)</span></li>
-              <li><div className="client-name">Omkar Altamount</div> <span className="location-tag">Malad</span></li>
-              <li><div className="client-name">Sunbeam Gurukurpa Developer</div> <span className="location-tag">Vile Parle(E)</span></li>
-              <li><div className="client-name">Raheja Legend Condominium</div> <span className="location-tag">Worli</span></li>
-              <li><div className="client-name">Lodha Crown</div> <span className="location-tag">Thane</span></li>
-              <li><div className="client-name">Sugan Heights</div> <span className="location-tag">Grand Road</span></li>
-              <li><div className="client-name">Palai Tower</div> <span className="location-tag">Goregaon</span></li>
-              <li><div className="client-name">Grand Pallazo</div> <span className="location-tag">Dadar</span></li>
-              <li><div className="client-name">VCA Square</div> <span className="location-tag">Dadar</span></li>
-              <li><div className="client-name">Kalpataru Magnus</div> <span className="location-tag">Bandra East</span></li>
-              <li><div className="client-name">Lodha Bellavue</div> <span className="location-tag">Byculla</span></li>
-              <li><div className="client-name">Saat Rasta properties pvt ltd</div> <span className="location-tag">Byculla</span></li>
-              <li><div className="client-name">Gundavali Co-operative Housing society LTD.</div> <span className="location-tag">Andheri</span></li>
-              <li><div className="client-name">Our select state bank of India supervising co-op HSG society</div> <span className="location-tag">Bandra</span></li>
-              <li><div className="client-name">Sea Kunal Corporation Pvt Ltd</div> <span className="location-tag">Colaba</span></li>
-            </ul>
-
+             <div className="table-container">
+                <table className="project-table border table-bordered w-75 mx-auto ongoing">
+                  <thead>
+                    <tr>
+                      <th>Sr.No</th>
+                      <th>Client Name</th>
+                      <th>Location</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr><td>1</td><td>Anubhav Restaurants</td><td>Fort</td></tr>
+                    <tr><td>2</td><td>South Indian Restaurants</td><td>Fort</td></tr>
+                    <tr><td>3</td><td>National Hindu Hotel</td><td>Fort</td></tr>
+                    <tr><td>4</td><td>Durga Bar and Restaurant</td><td>Andheri (E)</td></tr>
+                    <tr><td>5</td><td>Saifee Hospital</td><td>Charni Road</td></tr>
+                    <tr><td>6</td><td>Amar Fruit Juice Centre</td><td>Vile Parle(W)</td></tr>
+                    <tr><td>7</td><td>Amber Restaurant</td><td>Bandra(W)</td></tr>
+                    <tr><td>8</td><td>Urban Tadka</td><td>Malad (W)</td></tr>
+                    <tr><td>9</td><td>Hotel Silver Inn</td><td>Andheri (E)</td></tr>
+                    <tr><td>10</td><td>Shree Ambe</td><td>Andheri (E)</td></tr>
+                    <tr><td>11</td><td>Omkar Altamount</td><td>Malad</td></tr>
+                    <tr><td>12</td><td>Sunbeam Gurukurpa Developer</td><td>Vile Parle(E)</td></tr>
+                    <tr><td>13</td><td>Raheja Legend Condominium</td><td>Worli</td></tr>
+                    <tr><td>14</td><td>Lodha Crown</td><td>Thane</td></tr>
+                    <tr><td>15</td><td>Sugan Heights</td><td>Grand Road</td></tr>
+                    <tr><td>16</td><td>Palai Tower</td><td>Goregaon</td></tr>
+                    <tr><td>17</td><td>Grand Pallazo</td><td>Dadar</td></tr>
+                    <tr><td>18</td><td>VCA Square</td><td>Dadar</td></tr>
+                    <tr><td>19</td><td>Kalpataru Magnus</td><td>Bandra East</td></tr>
+                    <tr><td>20</td><td>Lodha Bellavue</td><td>Byculla</td></tr>
+                    <tr><td>21</td><td>Saat Rasta properties pvt ltd</td><td>Byculla</td></tr>
+                    <tr><td>22</td><td>Gundavali Co-operative Housing society LTD.</td><td>Andheri</td></tr>
+                    <tr><td>23</td><td>Our select state bank of India supervising co-op HSG society</td><td>Bandra</td></tr>
+                    <tr><td>24</td><td>Sea Kunal Corporation Pvt Ltd</td><td>Colaba</td></tr>
+                  </tbody>
+                </table>
+              </div>
+            {/* <OngoingList /> */}
+            <div className="projectlist d-none">
+              <ul className="ongoing">
+                <li><div className="client-name">Anubhav Restaurants</div> <span className="location-tag">Fort</span></li>
+                <li><div className="client-name">South Indian Restaurants</div> <span className="location-tag">Fort</span></li>
+                <li><div className="client-name">National Hindu Hotel</div> <span className="location-tag">Fort</span></li>
+                <li><div className="client-name">Durga Bar and Restaurant</div> <span className="location-tag">Andheri (E)</span></li>
+                <li><div className="client-name">Saifee Hospital</div> <span className="location-tag">Charni Road</span></li>
+                <li><div className="client-name">Amar Fruit Juice Centre</div> <span className="location-tag">Vile Parle(W)</span></li>
+                <li><div className="client-name">Amber Restaurant</div> <span className="location-tag">Bandra(W)</span></li>
+                <li><div className="client-name">Urban Tadka</div> <span className="location-tag">Malad (W)</span></li>
+                <li><div className="client-name">Hotel Silver Inn</div> <span className="location-tag">Andheri (E)</span></li>
+                <li><div className="client-name">Shree Ambe</div> <span className="location-tag">Andheri (E)</span></li>
+                <li><div className="client-name">Omkar Altamount</div> <span className="location-tag">Malad</span></li>
+                <li><div className="client-name">Sunbeam Gurukurpa Developer</div> <span className="location-tag">Vile Parle(E)</span></li>
+                <li><div className="client-name">Raheja Legend Condominium</div> <span className="location-tag">Worli</span></li>
+                <li><div className="client-name">Lodha Crown</div> <span className="location-tag">Thane</span></li>
+                <li><div className="client-name">Sugan Heights</div> <span className="location-tag">Grand Road</span></li>
+                <li><div className="client-name">Palai Tower</div> <span className="location-tag">Goregaon</span></li>
+                <li><div className="client-name">Grand Pallazo</div> <span className="location-tag">Dadar</span></li>
+                <li><div className="client-name">VCA Square</div> <span className="location-tag">Dadar</span></li>
+                <li><div className="client-name">Kalpataru Magnus</div> <span className="location-tag">Bandra East</span></li>
+                <li><div className="client-name">Lodha Bellavue</div> <span className="location-tag">Byculla</span></li>
+                <li><div className="client-name">Saat Rasta properties pvt ltd</div> <span className="location-tag">Byculla</span></li>
+                <li><div className="client-name">Gundavali Co-operative Housing society LTD.</div> <span className="location-tag">Andheri</span></li>
+                <li><div className="client-name">Our select state bank of India supervising co-op HSG society</div> <span className="location-tag">Bandra</span></li>
+                <li><div className="client-name">Sea Kunal Corporation Pvt Ltd</div> <span className="location-tag">Colaba</span></li>
+              </ul>
+            </div>
           </div>
         </div>
       </section>
